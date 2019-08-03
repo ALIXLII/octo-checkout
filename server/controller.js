715853e32dcb0b161ed4de17fd19fe4c95202c57
Alexander Lee <4lexslee@gmail.com>
2019-08-03 13:17:41 -0700

    Created routes for server

---
+++ server/controller.js
@@ -0,0 +1,11 @@
+module.exports = {
+  post: (req, res) => {
+    console.log("this is post");
+  },
+  get: (req, res) => {
+    console.log("this is a get");
+  },
+  put: (req, res) => {
+    console.log("this is a put");
+  }
+};

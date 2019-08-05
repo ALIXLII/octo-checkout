d94f21c6df98be536eeb8bdf44eb49d8f33928fa
Alexander Lee <4lexslee@gmail.com>
2019-08-05 10:57:49 -0700

    Patch request updates database

--- server/controller.js
+++ server/controller.js
@@ -3,11 +3,12 @@ const schema = require("../database/index.js")
 
 module.exports = {
   post: (req, res) => {
-    console.log("this is post");
+    console.log("this is a post");
     const octoCheckout = new schema({
       form1: {
         name: "alex",
-        email: "@mail"
+        email: "@mail",
+        password: "****"
       }
     }).save(err => {
       if (err) {
@@ -21,7 +22,22 @@ module.exports = {
     console.log("this is a get");
     res.send("get")
   },
-  put: (req, res) => {
-    console.log("this is a put");
+  patch: (req, res) => {
+    console.log("this is a patch");
+    schema.findOneAndUpdate(
+      "form1",
+      {
+        "form1.name": "bob",
+        "form1.email": "sdkhfksdh"
+      },
+      {new: true},
+      (err, doc) => {
+        if (err) {
+          console.log("error patching")
+        }
+        console.log(doc)
+        res.send("db patched")
+      }
+    )
   }
 };
